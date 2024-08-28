92ac65d9ad969330ebad5b20a42d15b0fa8e61c8
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-08-28 20:17:36 +0200

    Finish

--- middlewares/horaMiddleware.js
+++ middlewares/horaMiddleware.js
@@ -1,8 +1,9 @@
-
-
 const horaMiddleware = (req, res, next) => {
-    req.horaActual = new Date();
-    res.locals.horaActual = req.horaActual.toLocaleTimeString();
+    req.horaActual = new Date()
+    console.log(req.horaActual.toLocaleTimeString())
+    res.locals.horaActual = req.horaActual.toLocaleTimeString();//locals se asigna a res
+//facilita el paso de datos sin tener que usar funciones o métodos por separado
+    
     next();
 };
 
