228313420d1169a3d67f44026accde28b67bd477
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-08-29 19:49:07 +0200

    Finish

--- middlewares/validarHora.js
+++ middlewares/validarHora.js
@@ -3,7 +3,7 @@
 const validarHora = (req, res, next) => {
   const hora = req.horaActual.getHours();
   if (hora < 12) {
-      res.locals.mensaje = `Aún no son las 12 de la mañana, espera hasta las 12:00 para entrar`;
+      res.locals.mensaje = `Aún no son las 12 de la mañana, espera hasta las 00:00 para entrar`;
       return res.redirect('/?mensaje=' + encodeURIComponent(res.locals.mensaje));
   }
   next();
